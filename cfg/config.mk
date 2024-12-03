05f39b46e2ebd1193550703b225fa2e4bdfb27af
nickguimara <nickguimara@hotmail.com>
2024-12-03 14:44:21 -0300

    Update cfg
    Signed-off-by: nickguimara <nickguimara@hotmail.com>

--- cfg/config.mk
+++ cfg/config.mk
@@ -4,14 +4,14 @@ export PLATFORM    = ihp-sg13g2
 
 
 
-export VERILOG_FILES = /home/nickzus/i2c-gpio-expander/martin/src/filter.v \
-			           /home/nickzus/i2c-gpio-expander/martin/src/shreg.v \
-					   /home/nickzus/i2c-gpio-expander/martin/src/lfsr.v \
-		               /home/nickzus/i2c-gpio-expander/martin/src/martin_top.v
-export SDC_FILE      = /home/nickzus/i2c-gpio-expander/martin/cfg/constraint.sdc
-export FOOTPRINT_TCL = /home/nickzus/i2c-gpio-expander/martin/cfg/pad.tcl
+export VERILOG_FILES = ./../src/filter.v \
+			           ./../src/shreg.v \
+					   ./../src/lfsr.v \
+		               ./../src/martin_top.v
+export SDC_FILE      = ./constraint.sdc
+export FOOTPRINT_TCL = ./pad.tcl
 
-export SEAL_GDS = /home/nickzus/i2c-gpio-expander/martin/cfg/macros/sealring.gds
+export SEAL_GDS = ./macros/sealring.gds
 
 export FILL_CELLS = sg13g2_fill_1 sg13g2_fill_2 sg13g2_fill_4 sg13g2_fill_8 sg13g2_decap_4 sg13g2_decap_8
 
@@ -19,7 +19,7 @@ export DIE_AREA = 0.0 0.0 1370.0 1370.0
 export CORE_AREA = 390.0 390.0 980.0 980.0
 
 export TNS_END_PERCENT = 100
-export PDN_TCL = /home/nickzus/i2c-gpio-expander/martin/cfg/pdn.tcl
+export PDN_TCL = ./pdn.tcl
 
 export USE_FILL = 0
 
